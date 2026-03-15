c5861486128b555ac9b44aa97cc26c38964f7293
Zarif Ben Alam <zarifbenalam@gmail.com>
2026-03-15 13:19:52 +0600

    Update x11vnc listen address to 127.0.0.1
    Change x11vnc listen address to localhost for security.

--- start.sh
+++ start.sh
@@ -6,24 +6,26 @@ PORT="${PORT:-10000}"
 PASSWORD="${BROWSER_PASSWORD:-changeme123}"
 
 mkdir -p "$HOME/.vnc"
+mkdir -p /tmp/.X11-unix || true
+
 x11vnc -storepasswd "$PASSWORD" "$HOME/.vnc/passwd" >/dev/null
 
-Xvfb :1 -screen 0 1366x768x24 -ac -nolisten tcp &
-sleep 1
+Xvfb :1 -screen 0 1366x768x24 >/tmp/xvfb.log 2>&1 &
+sleep 2
 
 openbox >/tmp/openbox.log 2>&1 &
+
 x11vnc \
   -display :1 \
   -forever \
   -shared \
   -rfbauth "$HOME/.vnc/passwd" \
   -rfbport 5900 \
-  -listen 0.0.0.0 \
+  -listen 127.0.0.1 \
   >/tmp/x11vnc.log 2>&1 &
 
 sleep 2
 
 firefox-esr --display=:1 --no-remote --new-instance about:blank >/tmp/firefox.log 2>&1 &
 
-ln -sf /usr/share/novnc/vnc.html /usr/share/novnc/index.html
 exec websockify --web=/usr/share/novnc/ "$PORT" localhost:5900
